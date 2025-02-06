fb199a36d80509d38fc5a5d4c910e0fe78c9737e
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-02-06 15:37:10 -0800

    Making a treasure Island game

---
+++ treasureIsland.py
@@ -0,0 +1,10 @@
+print("Welcome to treasure island.")
+print("your mission is to find the treasure")
+move = input("you're at a cross road. Where do you want to go? Left or right?")
+if move == 'left':
+    print('You go to the left, you find a old rotten boat')
+    check = input('Do you want to check it?')
+
+elif move == 'right':
+    print('A skeleton appears! look out! has a sword!')
+
