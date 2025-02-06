2e4d9951dfb707ae7f28bee26f0ec22935b9775c
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-02-06 15:24:28 -0800

    adding username

--- hangman.py
+++ hangman.py
@@ -2,3 +2,7 @@ from random import choice
 
 def run_game():
     word: str = choice(['apple', 'secret', 'banana'])
+
+
+    username: str = input('What is your name? =>')
+    print(f'welcome to hangman')
