5c117e11898fee7490341e2f3f40b555df584048
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-02-06 16:09:59 -0800

    continuing and improving first part of hangman

--- hangman.py
+++ hangman.py
@@ -5,4 +5,25 @@ def run_game():
 
 
     username: str = input('What is your name? =>')
-    print(f'welcome to hangman')
+    print(f'welcome to hangman, {username}!')
+
+
+    # Set up
+    guessed: str = ''
+    tries: int = 3
+
+    while tries > 0:
+        blanks: int = 0
+
+        print('Word ', end='')
+        for char in word:
+            if char in guessed:
+                print(char, end='')
+            else: 
+                print('_', end='')
+                blanks+=1
+        print() # Adds a blank line
+
+        if blanks == 0:
+            print('you got it!')
+            break
