27f4488d2cf284ec5abb5320dce162c2e13476af
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-02-12 09:55:31 -0800

    Adding stuff in the mini-projects

---
+++ bandGenerator.py
@@ -0,0 +1,13 @@
+
+def main():
+    #This function prints a message
+    print("Welcome to the band name generator!")
+    #This makes a variable that keep the input.
+    cityName = input("What's the name of the city you grew up in?")
+    #This function prints the input
+    print(cityName)
+    petName = input("What's the name of your pet?")
+    
+    print(petName)
+    print("Your band name could be " + cityName + " " + petName)
+main()
