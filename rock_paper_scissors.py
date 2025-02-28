430e2a97a63660533234c59dcaa5fe87eb1f851e
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-02-28 09:56:39 -0800

    I am back!

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,2 +1,8 @@
 import random 
 import sys
+
+class RPS: 
+    def __init__(self):
+        print('Welcome to RPS 9000!')
+
+        self.moves: dict = {'rock': '🪨',  'paper': '📃' ,'scissors': '✂️'}
