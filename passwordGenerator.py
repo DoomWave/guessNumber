db7eed0f3fbab70e832c60174963e120664688b7
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-04-06 11:13:18 -0700

    password generator finished

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -10,12 +10,12 @@ def contains_upper(password: str) -> bool:
         return False
     
 
-    def contains_symbols(password: str) -> bool:
-        for char in password:
-            if char in string.punctuation:
-                return True
+def contains_symbols(password: str) -> bool:
+    for char in password:
+        if char in string.punctuation:
+            return True
         
-        return False
+    return False
 
 
 def generate_password(length: int, symbols: bool, uppercase: bool) -> str:
@@ -27,15 +27,17 @@ def generate_password(length: int, symbols: bool, uppercase: bool) -> str:
     if uppercase:
         combination += string.ascii_uppercase
 
-        combination_length = len()
-        new_password: str = ''
+    combination_length = len(combination)
+    new_password: str = ''
 
-        for _ in range(length):
-            new_password += combination[secrets.randbelow(combination_length)]
+    for _ in range(length):
+        new_password += combination[secrets.randbelow(combination_length)]
 
-        return new_password
+    return new_password
 
 
-
-    if __name__ == '__main__':
-        new_pass: str = generate_password(length=10, symbols=True, uppercase=False)
+if __name__ == '__main__':
+    for i in range(1, 6):
+        new_pass: str = generate_password(length=3, symbols=True, uppercase=True)
+        specs: str = f'U: {contains_upper(new_pass)}, S: {contains_symbols(new_pass)}'
+        print(f'{i} -> {new_pass} ({specs})')
